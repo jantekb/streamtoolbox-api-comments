b7a3f7141bc9c2b0e8fdc796c7297bad3235a217
jantekb <jantekb@gmail.com>
2015-03-14 21:24:43 +0100

    Rebranding

--- src/com/wowza/wms/httpstreamer/model/IHTTPStreamerSession.java
+++ src/com/wowza/wms/httpstreamer/model/IHTTPStreamerSession.java
@@ -2,4 +2,14 @@ package com.wowza.wms.httpstreamer.model;
 
 public interface IHTTPStreamerSession {
 
+	/**
+	 * Never seen it being false, it is true even after you call {@see #rejectSession()}
+	 * @return apparently always just true (?)
+	 */
+	boolean isActive();
+	
+	/**
+	 * It can be used to get rid of the player using this session, the player gets HTTP 403 when it asks for the next media chunk.
+	 */
+	void rejectSession();
 }
