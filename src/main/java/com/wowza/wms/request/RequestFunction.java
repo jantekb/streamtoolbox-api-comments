7ebf67adcf9a75659c598014c5dee60a60f68fed
hegedusi <isti9892@gmail.com>
2015-08-04 14:35:29 +0200

    More doc

--- src/main/java/com/wowza/wms/request/RequestFunction.java
+++ src/main/java/com/wowza/wms/request/RequestFunction.java
@@ -1,5 +1,182 @@
 package com.wowza.wms.request;
 
-public class RequestFunction {
+import java.nio.ByteBuffer;
 
+import com.wowza.wms.amf.AMFDataList;
+
+public class RequestFunction {
+	
+	/**
+	 * 
+	 */
+	public RequestFunction() {
+		
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public byte[] getData() {
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public ByteBuffer getDataByteBuffer() {
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getSize() {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getMissing() {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @param paramInt
+	 */
+	public void setSize(int paramInt) {
+		
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getSrc() {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @param paramInt
+	 */
+	public void setSrc(int paramInt) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param paramArrayOfByte
+	 * @param paramInt1
+	 * @param paramInt2
+	 */
+	public void addData(byte[] paramArrayOfByte, int paramInt1, int paramInt2) {
+		
+	}
+	  
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean isPartial() {
+		return false;
+	}
+	  
+	/**
+	 * 
+	 * @return
+	 */
+	public AMFDataList getMessage() {
+		return null;
+	}
+	
+	/**
+	 * 
+	 */
+	public String toString() {
+		return null;
+	}
+	/**
+	 *   
+	 * @return
+	 */
+	public int getType() {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @param paramInt
+	 */
+	public void setType(int paramInt) {
+		
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public long getTimecode() {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @param paramLong
+	 */
+	public void setTimecode(long paramLong) {
+		
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getWmsNumber() {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @param paramInt
+	 */
+	public void setWmsNumber(int paramInt) {
+		
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public ByteBuffer getExtraData() {
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param paramByteBuffer
+	 */
+	public void setExtraData(ByteBuffer paramByteBuffer) {
+		
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getHeaderSize() {
+		return 0;
+	}
+
+	/**
+	 * 
+	 * @param paramInt
+	 */
+	public void setHeaderSize(int paramInt) {
+		
+	}
 }
