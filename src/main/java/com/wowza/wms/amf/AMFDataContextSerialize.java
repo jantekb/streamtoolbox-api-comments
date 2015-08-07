ecc1ab760137af4f02bd0d8f07574e78b8639092
hegedusi <isti9892@gmail.com>
2015-08-07 19:18:10 +0200

    More doc

--- src/main/java/com/wowza/wms/amf/AMFDataContextSerialize.java
+++ src/main/java/com/wowza/wms/amf/AMFDataContextSerialize.java
@@ -1,5 +1,106 @@
 package com.wowza.wms.amf;
 
-public class AMFDataContextSerialize {
+import java.io.DataOutputStream;
 
+public class AMFDataContextSerialize {
+	
+	/**
+	 * 
+	 */
+	public AMFDataContextSerialize() {
+		
+	}
+	
+	/**
+	 * 
+	 * @param paramInt
+	 */
+	public AMFDataContextSerialize(int paramInt) {
+		
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getObjectEncoding() {
+		return 0;
+	}
+	
+	 /**
+	  * 
+	  * @param paramInt
+	  */
+	public void setObjectEncoding(int paramInt) {
+		
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getTargetEncoding() {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @param paramInt
+	 */
+	public void setTargetEncoding(int paramInt) {
+		
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean isAMF3() {
+		return false;
+	}
+	  
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean isAMF0() {
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param paramString
+	 * @return
+	 */
+	public int getStringReference(String paramString) {
+		return 0;
+	}
+	  
+	/**
+	 * 
+	 * @param paramObject
+	 * @return
+	 */
+	public int getObjectReference(Object paramObject) {
+		return 0;
+	}
+	  
+	/**
+	 * 
+	 * @param paramAMFDataTrait
+	 * @return
+	 */
+	public int getTraitReference(AMFDataTrait paramAMFDataTrait) {
+		return 0;
+	}
+	  
+	/**
+	 * 
+	 * @param paramDataOutputStream
+	 * @param paramString
+	 */
+	public void writeString(DataOutputStream paramDataOutputStream, String paramString) {
+		
+	}
+	  
 }
