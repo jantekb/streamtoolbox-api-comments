2d7bef5538149c057917f3213ba124a63ac88280
jantekb <jantekb@gmail.com>
2015-10-13 23:15:50 +0200

    ReleaseInfo class added

---
+++ src/main/java/com/wowza/wms/server/ReleaseInfo.java
@@ -0,0 +1,21 @@
+package com.wowza.wms.server;
+
+public class ReleaseInfo {
+
+	/**
+	 * Returns the version string of the engine. Missing from documentation (4.3) but mentioned in an example.
+	 * 
+	 * @return the version of the Wowza Streaming Engine, e.g. "3.5", "4.1"
+	 */
+	public static String getVersion() {
+		return null;
+	}
+	
+	/**
+	 * Returns the build number of the engine. Missing from documentation (4.3) but mentioned in an example.
+	 * @return the build number of Wowza Streaming Engine
+	 */
+	public static String getBuildNumber() {
+		return null;
+	}
+}
